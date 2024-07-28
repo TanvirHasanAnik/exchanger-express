e6aec844a3cd660623a89847fac580627a7782b5
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2024-07-28 18:07:58 +0600

    product list gets product title and description too

--- controllers/userProductsController.js
+++ controllers/userProductsController.js
@@ -1,7 +1,7 @@
 var connection = require('../connection');
 
 function productsList(req,res){
-    connection.query('SELECT categoryname from category inner join products on products.categoryid = category.id where products.userid = ?',[req.params.id],(err,rows)=>{
+    connection.query('SELECT categoryname,productTitle,productDescription from category inner join products on products.categoryid = category.id where products.userid = ?',[req.params.id],(err,rows)=>{
     
     if(err){
         console.log(err);
