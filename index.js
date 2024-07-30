8363b72fc8359d88bb3c2240fdf3d743bfecbfe8
Tanvir Hassan Anik <tanvir.h.anik@gmail.com>
2024-07-30 16:28:21 +0600

    set cors allow origin to localhost

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', ['null']);
+    res.header('Access-Control-Allow-Origin', ['http://localhost']);
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
     res.header('Access-Control-Allow-Credentials', 'true');
